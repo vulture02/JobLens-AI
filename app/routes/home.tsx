29e5a746638a77ddcef603726e08e53ed91c53cc
vulture02 <amithp0210@gmail.com>
2025-08-10 11:50:40 +0530

    Auth

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,10 @@
 import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
 import { resumes } from "../../constants";
-import ResumeCard from "~/components/resumecard";
+import ResumeCard from "~/components/ResumeCard";
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -11,9 +14,17 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+   const { auth } = usePuterStore();
+    const navigate=useNavigate();
+    useEffect(()=>{
+      if(!auth.isAuthenticated) navigate('/auth?next=/');
+  
+    },[auth.isAuthenticated]);
+  
   return (
     <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
       <Navbar />
+    
       <section className="flex flex-col items-center gap-8 pt-12 max-sm:mx-2 mx-15 pb-5 py-16">
         <div className="flex flex-col items-center gap-8 max-w-4xl text-center max-sm:gap-4">
           <h1>Track your Application & Resume Ratings</h1>
