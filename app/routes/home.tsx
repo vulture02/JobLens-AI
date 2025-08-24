7f5983c395de731f90d2d3961ffbe80fbb5d36c2
vulture02 <amithp0210@gmail.com>
2025-08-24 22:13:24 +0530

    home page

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,44 +1,81 @@
-import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { resumes } from "../../constants";
+import Navbar from "~/components/Navbar";
 import ResumeCard from "~/components/ResumeCard";
-import { useEffect } from "react";
-import { useLocation, useNavigate } from "react-router";
-import { usePuterStore } from "~/lib/puter";
+import {usePuterStore} from "~/lib/puter";
+import {Link, useNavigate} from "react-router";
+import {useEffect, useState} from "react";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "resumemind" },
-    { name: "description", content: "smart feedback for your dream job" },
+    { title: "Resumind" },
+    { name: "description", content: "Smart feedback for your dream job!" },
   ];
 }
 
 export default function Home() {
-   const { auth } = usePuterStore();
-    const navigate=useNavigate();
-    useEffect(()=>{
-      if(!auth.isAuthenticated) navigate('/auth?next=/');
-  
-    },[auth.isAuthenticated]);
-  
-  return (
-    <main className="bg-[url('/images/bg-main.svg')] bg-cover bg-center">
-      <Navbar />
-    
-      <section className="flex flex-col items-center gap-8 pt-12 max-sm:mx-2 mx-15 pb-5 py-16">
-        <div className="flex flex-col items-center gap-8 max-w-4xl text-center max-sm:gap-4">
-          <h1>Track your Application & Resume Ratings</h1>
-          <h2>Review your submission and check AI-powered feedback</h2>
-        </div>
-      
-      {resumes.length > 0 && (
-        <div className="flex flex-wrap max-md:flex-col max-md:gap-4 gap-6 items-start max-md:items-center w-full max-w-[1850px] justify-evenly">
+  const { auth, kv } = usePuterStore();
+  const navigate = useNavigate();
+  const [resumes, setResumes] = useState<Resume[]>([]);
+  const [loadingResumes, setLoadingResumes] = useState(false);
+
+  useEffect(() => {
+    if(!auth.isAuthenticated) navigate('/auth?next=/');
+  }, [auth.isAuthenticated])
+
+useEffect(() => {
+  const loadResumes = async () => {
+    setLoadingResumes(true);
+
+    const resumes = (await kv.list('resume:*', true)) as KVItem[];
+
+    // Parse and remove duplicates based on resume.id
+    const parsedResumes = resumes
+      ?.map((resume) => JSON.parse(resume.value) as Resume)
+      .filter((value, index, self) =>
+        index === self.findIndex((r) => r.id === value.id)
+      );
+
+    setResumes(parsedResumes || []);
+    setLoadingResumes(false);
+  }
+
+  loadResumes();
+}, []);
+
+
+  return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+    <Navbar />
+
+    <section className="main-section">
+      <div className="page-heading py-16">
+        <h1>Track Your Applications & Resume Ratings</h1>
+        {!loadingResumes && resumes?.length === 0 ? (
+            <h2>No resumes found. Upload your first resume to get feedback.</h2>
+        ): (
+          <h2>Review your submissions and check AI-powered feedback.</h2>
+        )}
+      </div>
+      {loadingResumes && (
+          <div className="flex flex-col items-center justify-center">
+            <img src="/images/resume-scan-2.gif" className="w-[200px]" />
+          </div>
+      )}
+
+      {!loadingResumes && resumes.length > 0 && (
+        <div className="resumes-section">
           {resumes.map((resume) => (
-            <ResumeCard key={resume.id} resume={resume} />
+              <ResumeCard key={resume.id} resume={resume} />
           ))}
         </div>
       )}
-      </section>
-    </main>
-  );
+
+      {!loadingResumes && resumes?.length === 0 && (
+          <div className="flex flex-col items-center justify-center mt-10 gap-4">
+            <Link to="/upload" className="primary-button w-fit text-xl font-semibold">
+              Upload Resume
+            </Link>
+          </div>
+      )}
+    </section>
+  </main>
 }
